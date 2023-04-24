7507c466dc7f48d191538fa546c079576bbb78cd
Francesco Marchiori <francesco.marchiori98@gmail.com>
2023-04-24 11:28:17 +0200

    Added results save capability

--- utils/const.py
+++ utils/const.py
@@ -6,27 +6,10 @@ SEED = 151836
 # Folders
 RAW = './Datasets/Raw/'
 PROCESSED = './Datasets/Processed/'
+RESULTS = './Results/'
+FIGURES = './Results/Figures/'
 
 # Datasets
-# DATASETS = [
-#     'Berkley',
-#     'CALCE_1',
-#     'CALCE_2',
-#     'CALCE_3',
-#     'CALCE_4',
-#     'CALCE_5',
-#     'EVERLASTING_1',
-#     'EVERLASTING_2',
-#     'HNEI',
-#     'OX',
-#     'OX_1',
-#     'OX_2',
-#     'SNL',
-#     'TRI_1',
-#     # 'TRI_2',
-#     'UCL',
-#     'UL'
-# ]
 DATASETS = sorted(os.listdir(PROCESSED))
 
 MIN_LENGTH = 90
